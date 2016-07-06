2e5f5ffee5d98989ea60e6394021cf0ec73504e2
Alexis <alexiseliudrdz@gmail.com>
2016-07-06 12:08:09 -0500

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+
+  def index
+  	@titulo = "Curso Rails - Index"
+  end
+
+  def quienes_somos
+  	@titulo = "Curso Rails - Quienes somos"
+
+  end
+
+  def contacto
+  	@titulo = "Curso Rails - Contacto"
+  end
+  def localizacion
+  	@titulo = "Curso Rails - Localizacion"
+  end
+  def servicios
+    if request.get?
+      @titulo = "Curso Rails - Servicios - Get"      
+    else
+      @titulo = "Curso Rails - Servicios - Post"  
+    end
+  end
+end
